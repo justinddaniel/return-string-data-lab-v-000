ad908aa38520255640316e543555e037e2220e22
Justin Daniel <justinddaniel@gmail.com>
2018-02-08 00:00:23 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   root 'products#index'
   resources :products
-  resources :orders
-  resources :invoices
   get 'products/:id/body', to: 'products#body'
   get 'products/:id/available?', to: 'products#available?'
+  resources :orders
+  resources :invoices
+
 end
